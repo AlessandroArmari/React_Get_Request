f5666e4ac6d8b792da0bca13fd68c53f6074b212
hobisognodiunarma <alessandroarmari22@gmail.com>
2023-01-19 17:11:46 +0100

    in medias res

--- React_Get_Request/src/App.jsx
+++ React_Get_Request/src/App.jsx
@@ -1,15 +1,40 @@
-import { Order } from "./components/Order";
+//---> follow the numbers!
+
 import { OrderList } from "./components/OrderList";
 
 function App() {
-  const orderList = [
-    { id: "1", name: "Pizza", price: "8.9" },
-    { id: "2", name: "Pane", price: "1.2" },
-  ];
+  /*2*/
+  const fetchOrderHandler = async () => {
+    //setIsLoading(true);
+    //setError(null);
+
+    try {
+      const response = await fetch(
+        // ---> the return of fetch() is a Promise (here called: response)
+        //---> await= wait for the Promise result (the data stored in this URL!)
+        "https://1ed3f2d0-25a5-433b-97a4-40290bff2df6.mock.pstmn.io/myorders"
+      );
+      console.log(response);
+
+      // --->response has a "property "ok"
+      if (!response.ok) {
+        //--->if the answer if different from 200 (ok), cast an error!
+        throw new Error("Something went wrong!");
+      }
+
+      const data = await response.json(); //--->I'm make my response a .json file ---> ("readable")
+      //--->+++ACTUALLY: it returns a Promise--->a JavaScript object that can be rapresented by JSON (developer.mozilla)
+    } catch {}
+  };
+
   return (
-    <div>
-      <OrderList orders={orderList} />
-    </div>
+    <>
+      <section>
+        {/*1*/}
+        <button onClick={fetchOrderHandler}>Fetch Order</button>
+      </section>
+      <section></section>
+    </>
   );
 }
 
@@ -19,11 +44,13 @@ export default App;
   as "props" of "orders" too see how list works
 
   In the actual exercise we "fetch" the "list" from 
-  a server...here, from a list of object...
+  a server (which give us a Json which is similar to this object)
+  ...here, from a list of object...
   
   
   const orderList = [
     { id: "1", name: "Pizza", price: "8.9" },
     { id: "2", name: "Pane", price: "1.2" },
+    { id: "44", name: "Tomato", price: "3.5" },
   ];
   */
